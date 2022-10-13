f20cf5bde8e299dc5bfaa6c399f78756aff450f6
KimByeungHyun <qudgus05@naver.com>
2022-10-13 20:17:13 +0900

    항해99_숙련주차_과제제출

--- src/main/java/com/sparta/submitHomwork/controller/CommentController.java
+++ src/main/java/com/sparta/submitHomwork/controller/CommentController.java
@@ -36,27 +36,4 @@ public class CommentController {
         commentService.delete(id);
         return id;
     }
-
-
-    /*//모든 글 읽어 오기
-    @GetMapping("/dev/comment")
-    public List<Comment> getAllpost(){return commentService.getAllpost();}
-
-    //댓글 쓰기
-    @PostMapping("/dev/comment/{id}")
-    public Comment createComment(@RequestBody CommentRequestDto requestDto) {
-        return commentService.createComment(requestDto);
-    }
-
-    //댓글 수정
-    @PutMapping("/dev/comment/{id}")
-    public Optional<Comment> updateComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long id){
-        return commentService.updateComment(requestDto, id);}
-
-
-    //댓글 삭제
-    @DeleteMapping("/dev/comment/{id}")
-    public void deleteComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long id) {
-        commentService.deleteComment(requestDto,id);
-    }*/
 }
